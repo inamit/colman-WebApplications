5cf16aa280f53afd3231a0a335880406e0d599c1
Benli <@example.com>
2025-01-03 16:52:27 +0200

    fix "expiresIn"  field in token creation - cr note

--- src/utilities/token.ts
+++ src/utilities/token.ts
@@ -1,11 +1,12 @@
+import ms from "ms";
 import jwt from "jsonwebtoken";
 import { Response } from "express";
 import { IUser } from '../models/users_model';
 
 export const generateTokens = async (user: IUser) => {
-    const accessToken = jwt.sign({ "_id": user._id }, String(process.env.TOKEN_SECRET), { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION_MILLISECONDS });
+    const accessToken = jwt.sign({ "_id": user._id }, String(process.env.TOKEN_SECRET), { expiresIn: ms(Number(process.env.ACCESS_TOKEN_EXPIRATION_MILLISECONDS)) });
     const random = Math.floor(Math.random() * 1000000).toString();
-    const refreshToken = jwt.sign({ "_id": user._id, "random": random }, String(process.env.TOKEN_SECRET), {expiresIn: process.env.REFRESH_TOKEN_EXPIRATION_MILLISECONDS});
+    const refreshToken = jwt.sign({ "_id": user._id, "random": random }, String(process.env.TOKEN_SECRET), { expiresIn: ms(Number(process.env.REFRESH_TOKEN_EXPIRATION_MILLISECONDS)) });
     return {accessToken, refreshToken};
 }
 
